bfb4ab139118620a1bdc30076f723e419e22ab7d
Cindy <cindy_singer913@msn.com>
2026-01-17 10:44:14 +0100

    modif

--- assets/js/loader.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Masquer le loader une fois que la page est chargée
-    const loader = document.getElementById('loader');
-    if (loader) {
-        loader.style.display = 'none';
-    }
-});
